b0a3f73284e7109441d02b8ce894661e1134da3e
EuUst <atokadota2@yandex.ru>
2022-11-12 18:51:57 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LessonTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
