7569d8ead7bd6d0d98f50cbe47ff739d65b9a998
FazylovV <vladislav.fazylov04@gmail.com>
2022-11-12 20:39:24 +0500

    Complete 1 and 2 tasks

---
+++ ArraySorter.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LessonTask
+{
+    internal class ArraySorter
+    {
+        public static string[][] GetArrayContinent(string[] array, string continent)
+        {
+            var list = new List<string[]>();
+            foreach (string line in array)
+            {
+                string[] props = line.Split(';');
+                if (props[4] == continent) list.Add(props);
+            }
+
+            return list.ToArray();
+        }
+
+        public static string[] GetSortedArray(string[][] linesOfProps)
+        {
+            var sortedArray = new List<string>();
+            for (int i = 0; i < linesOfProps.Length; i++)
+            {
+                int min = 2000000000;
+                int index = 0;
+
+                for (int j = 0; j < linesOfProps.Length; j++)
+                {
+                    int population = Convert.ToInt32(linesOfProps[j][3]);
+                    if (population < min)
+                    {
+                        min = population;
+                        index = j;
+                    }
+
+                    if (j == linesOfProps.Length - 1) sortedArray.Add(String.Join(';', linesOfProps[index]));
+                }
+
+                linesOfProps[index][3] = "2000000000";
+            }
+
+            return sortedArray.ToArray();
+        }
+    }
+}
