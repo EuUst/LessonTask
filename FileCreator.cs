0bed3baee9d8a7b631080ba78df2b660e8c0d24d
Parfen774 <113260962+Parfen774@users.noreply.github.com>
2022-11-12 19:39:43 +0500

    Создаёт файл и вписывает строку

---
+++ FileCreator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+
+namespace LessonTask
+{
+    internal class FileCreator
+    {
+        public static void CreateFile(string firstLine)
+        {
+            Console.Write("Введите название файла: ");
+            string name = Console.ReadLine();
+
+            StreamWriter file = new StreamWriter(name + ".txt");
+            file.Write(firstLine);
+            file.Close();
+        }
+    }
+}
